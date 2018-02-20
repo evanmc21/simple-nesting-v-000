4b0b666cf6c8ddbac367f371e0eb641e8f726f29
Evan Candler <emcandler21@gmail.com>
2018-02-20 18:55:30 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -5,10 +5,17 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
         }
-
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
